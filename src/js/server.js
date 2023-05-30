84c1a544c283364b756c197c7229f8602e811557
GreyRuler <daniil_vasilenko757@mail.ru>
2023-05-30 09:03:15 +0300

    fix port ws

--- src/js/server.js
+++ src/js/server.js
@@ -158,7 +158,9 @@ router.post('/favourites/:id', async (ctx) => {
 	ctx.status = 200;
 });
 
-const wsServer = new WebSocketServer({});
+const wsServer = new WebSocketServer({
+	port: 3030,
+});
 
 function sendAllUsers(message) {
 	Array.from(wsServer.clients)
